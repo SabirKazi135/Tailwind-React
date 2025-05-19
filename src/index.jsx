5ef805b9d3fbddc53a3fd22b9a1acbf0f0dc66a3
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-19 14:10:51 +0530

    commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,22 @@
 function Index() {
   return (
     <div>
-      <div className="px-8 py-12 w-1/3">
+      <div className="px-8 py-12 w-1/3 space-y-6">
         <img
           className="h-10"
           src="../public/img/logo-brand.svg"
           alt="Workcation"
         />
         <img
-          className="mt-6 rounded-lg shadow-xl"
+          className="mt- rounded-lg shadow-xl"
           src="../public/img/beach-work.jpg"
           alt=""
         />
-        <h1 className="mt-6 text-2xl font-bold text-gray-900">
+        <h1 className="mt- text-2xl font-bold text-gray-900">
           You can work from anywhere.{" "}
           <span className="text-indigo-500"> Take advantage of it.</span>
         </h1>
-        <p className="mt-2">
+        <p className="mt-">
           Workcation helps you find work-friendly rentals in beautiful locations
           so you can enjoy some nice weather even when you're not on vacation.
         </p>
