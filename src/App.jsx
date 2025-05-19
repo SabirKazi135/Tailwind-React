2d99829f52814ac071d424e3a772fc87a6b6dcfc
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-19 13:09:59 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-gray-900 text-gray-200 inline-block px-4 py-2 rounded-[6px] ml-16 mt-4">
+        Hello Sabir
+      </h1>
+    </>
+  );
+}
+
+export default App;
