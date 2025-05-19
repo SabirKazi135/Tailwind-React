a74bb5cf4c4861188162310d3fe0195e38b17310
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-19 13:45:54 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Index from ".";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="bg-gray-900 text-gray-200 inline-block px-4 py-2 rounded-[6px] ml-16 mt-4">
-        Hello Sabir
-      </h1>
+      <Index />
     </>
   );
 }
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+function Index() {
+  return (
+    <>
+      <h1 className="">Hello World</h1>
+    </>
+  );
+}
+
+export default Index;
