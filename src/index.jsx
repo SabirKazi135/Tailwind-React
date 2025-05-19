21d4eb9ee1cff7174f9df2ec5e1256061b6abf07
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-19 15:16:07 +0530

    commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,28 @@
 function Index() {
   return (
     <div>
-      <div className="px-8 py-12 max-w-md mx-auto">
+      <div className="px-8 py-12 max-w-md mx-auto sm:max-w-xl">
         <img
           className="h-10"
           src="../public/img/logo-brand.svg"
           alt="Workcation"
         />
         <img
-          className="mt-6 rounded-lg shadow-xl"
+          className="mt-6 sm:mt-8 rounded-lg sm:h-64 sm:w-full sm:object-cover sm:object-center shadow-xl"
           src="../public/img/beach-work.jpg"
           alt=""
         />
-        <h1 className="mt-6 text-2xl font-bold text-gray-900">
+        <h1 className="mt-6 sm:mt-8 text-2xl font-bold sm:text-4xl text-gray-900">
           You can work from anywhere.{" "}
           <span className="text-indigo-500"> Take advantage of it.</span>
         </h1>
-        <p className="mt-4 text-gray-600">
+        <p className="mt-2 sm:mt-4 sm:text-xl text-gray-600">
           Workcation helps you find work-friendly rentals in beautiful locations
           so you can enjoy some nice weather even when you're not on vacation.
         </p>
-        <div className="mt-4">
+        <div className="mt-4 sm:mt-6">
           <a
-            className="uppercase tracking-wider font-semibold text-sm bg-indigo-500 rounded-lg text-white inline-block px-5 py-3 shadow-lg"
+            className="uppercase sm:text-base tracking-wider font-semibold text-sm bg-indigo-500 rounded-lg text-white inline-block px-5 py-3 shadow-lg"
             href="#"
           >
             Book Your escape
