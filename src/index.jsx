fb4b74da5fd06befa5fd15f02d56adab7a830d73
sabirkazi135 <sabirkazi135@gmail.com>
2025-05-20 15:21:49 +0530

    commit

--- src/index.jsx
+++ src/index.jsx
@@ -24,10 +24,7 @@ function Index() {
             vacation.
           </p>
           <div className="mt-4 sm:mt-6">
-            <a
-              className="inline-block px-5 py-3 rounded-lg shadow-lg bg-indigo-500 hover:bg-indigo-400 hover:-translate-0.5 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-indigo-500 focus:ring-opacity-50 active:bg-indigo-600 transform transition text-white uppercase tracking-wider font-semibold text-sm sm:text-base"
-              href="#"
-            >
+            <a className="btn" href="#">
               Book Your escape
             </a>
           </div>
